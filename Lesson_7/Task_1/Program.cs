afa70de2560b16316b2911a7f6340c36d81ece18
Даниил Русских <daniilrusskihhh@gmail.com>
2024-03-09 19:34:18 +0300

    написал привет Патуит)

--- Lesson_7/Task_1/Program.cs
+++ Lesson_7/Task_1/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Task_1
+﻿using System.Collections.Concurrent;
+
+namespace Task_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Что ж попытка 2!");
+            Console.WriteLine("privet Patuit!"); ;
         }
     }
 }
