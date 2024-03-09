e790dabf800f0cea17851d463bc2fcf664c57ae1
Даниил Русских <daniilrusskihhh@gmail.com>
2024-03-09 19:37:15 +0300

    исправил что ж

--- Lesson_7/Task_1/Program.cs
+++ Lesson_7/Task_1/Program.cs
@@ -6,7 +6,7 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Что ж попытка 2!");
+            Console.WriteLine("Что ж...!");
             Console.WriteLine("privet Patuit!");
         }
     }
