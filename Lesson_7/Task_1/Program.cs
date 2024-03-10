9efffe5ccf6d5bbff2651ba88e041a9e86cfb5bc
Даниил Русских <daniilrusskihhh@gmail.com>
2024-03-10 23:03:18 +0300

    решил домашнее задание по теме 7

--- Lesson_7/Task_1/Program.cs
+++ Lesson_7/Task_1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Concurrent;
+using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Task_1
 {
@@ -6,8 +8,49 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Что ж...!");
-            Console.WriteLine("privet Patuit!");
+            /*Два треугольника заданы длинами своих сторон. 
+             * Определить, площадь какого из них больше
+             * (создать метод для вычисления  площади  треугольника по длинам его сторон).  
+             * Для решения задачи можно использовать формулу Герона */
+            Console.WriteLine("введите стороны первого треугольника");
+            double a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());
+            double c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите стороны второго треугольника");
+            double a1 = Convert.ToInt32(Console.ReadLine());
+            double b1 = Convert.ToInt32(Console.ReadLine());
+            double c1 = Convert.ToInt32(Console.ReadLine());
+
+            Find_S_triangle(a, b, c, a1, b1, c1);
+
+            static void Find_S_triangle(double a, double b, double c, double a1, double b1, double c1)
+            {
+                double p = (a + b + c) / 2;
+                double s = Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 2);//с округлением до двух знаком после запятой
+                Console.WriteLine($"площадь первого треугольника:{s}");
+
+                double p1 = (a1 + b1 + c1) / 2;
+                double s1 = Math.Round(Math.Sqrt(p1 * (p1 - a1) * (p1 - b1) * (p1 - c1)), 2);
+                Console.WriteLine($"площадь второго треугольника:{s1}");
+
+                if (s > s1)
+                {
+                    Console.WriteLine("площадь первого треугольника больше");
+                    // Код для случая, когда s > s1
+                }
+                else if (s < s1)
+                {
+                    Console.WriteLine("площадь второго треугольника больше");
+                    // Код для случая, когда s < s1
+                }
+                else
+                {
+                    Console.WriteLine("у трекгольников одинаковая площадь");
+                    // Код для случая, когда s == s1
+                }
+            };
         }
     }
 }
+
---
+++ Lesson_7/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите сторону куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double ploshad, objem; // без инициализации можно передать через out
+                                   // а ref используем, когда  нам нужно, чтобы результат изменения переменной внутри метода можно было использовать вне метода
+                                   // мы передаем параметр по ссылке, и метод не будет делать копию параметра, а будет изменять его по ммылке (это мне напоминалочка)
+            CUBE_square_volume(a, out ploshad, out objem);
+
+            static void CUBE_square_volume(double a, out double volume, out double s )// внутри метра параметры могут называться по другому, главное порядок передачи
+            {
+                 volume = Math.Pow(a, 3);
+                 s = 6 * (a * a);         
+            }
+
+
+            Console.WriteLine($"площадь куба {ploshad}");
+            Console.WriteLine($"объем куба {objem}");
+           ;
+        }
+    }
+}
